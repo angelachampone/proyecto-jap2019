f4d2d7c8c59bc775146795dcfb67ac28e467961c
angela <angela.champone@gmail.com>
2019-10-20 23:28:54 -0300

    3er entrega

--- js/cart.js
+++ js/cart.js
@@ -10,7 +10,7 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
-    let unitProductCostHTML = document.getElementById("productCostText");
+    /*let unitProductCostHTML = document.getElementById("productCostText");
     let comissionCostHTML = document.getElementById("comissionText");
     let totalCostHTML = document.getElementById("totalCostText");
 
@@ -20,15 +20,39 @@ function updateTotalCosts(){
 
     unitProductCostHTML.innerHTML = unitCostToShow;
     comissionCostHTML.innerHTML = comissionToShow;
-    totalCostHTML.innerHTML = totalCostToShow;
+    totalCostHTML.innerHTML = totalCostToShow;*/
 }
 
-function updateSubtotal(unitario, cProductos){
+function updateSubtotal(){
     let preUni = parseInt(document.getElementById("precio").textContent);
-    let cant = parseInt(document.getElementById("cantidad").textContent);
+    let cant = parseInt(document.getElementById("cantidad").value);
     let subTotal = cant * preUni;
     document.getElementById("subTotal").innerHTML = subTotal;
+
+    }
+
+function showArticles(array){
+    let contenido = "";
+    for(let i = 0; i < array.length; i++){
+        let producto = array[i];
+    contenido +=`
+        <tr>
+            <td><img src= "` + producto.src + `" width:="20px"></td>
+            <td>` + producto.name +`</td>
+            <td id="precio"> ` + producto.unitCost + `</td>
+            <td>` + producto.currency + `</td>
+            <td><input type="number" name="count" id="cantidad" value=` + producto.count + `></td>
+            <td><span id="subTotal"</span></td>
+        </tr>
+    `
+     document.getElementById("producto").innerHTML = contenido;
+        updateSubtotal();
+    document.getElementById("cantidad").addEventListener("change",function(){
+        updateSubtotal();
+    })
 }
+}
+
 
 function showPaymentTypeNotSelected(){
 
@@ -38,23 +62,6 @@ function hidePaymentTypeNotSelected(){
 
 }
 
-function showArticles(articles){
-    let contenido = "";
-    contenido +=`
-        <tr>
-            <td><img src="img/tree1.jpg" width:="20px"></td>
-            <td>Pino de olor</td>
-            <td id="precio">100</td>
-            <td>UYU</td>
-            <td input type="numbrer" name="count" id="cantidad" value="2"></td>
-            <td><span id="subTotal"</span></td>
-      
-        </tr>
-    `
-    document.getElementById("productsTable").innerHTML = contenido;
-    updateSubtotal();
-}
-
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -62,7 +69,8 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO_URL).then(function(resultObj){
         if (resultObj.status=== "ok")
         {
-            showArticles();
+            showArticles(resultObj.data.articles);
         }
-    }
-});
+    });
+})
+
